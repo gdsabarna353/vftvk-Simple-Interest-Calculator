11fc250d9fcc87b0df5600216650747a8b7f68de
Sabarna Ghosh Dastidar <73946046+gdsabarna353@users.noreply.github.com>
2021-04-10 20:41:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+   var Amount = document.getElementById("principal");
+   var Rate = document.getElementById("rate");
+   var Years = document.getElementById("years");
+   if(Amount.value<=0){
+       alert("please enter the positive value of Amount");
+       Amount.focus();
+       return false;
+   }
+   if(Rate.value==0){
+    alert("please enter a positive value of Rate");
+    Rate.focus();
+    return false;
 }
+
+   var final = parseFloat(Amount.value) * parseFloat(Rate.value) * parseInt(Years.value) + parseFloat(Amount.value);
+   Years = 2020+parseInt(Years.value);
+   document.getElementById("result").innerHTML="If you deposit Rs."+"<mark>"+Amount.value+"</mark>"+"<br>"+"at an interest rate of "+"<mark>"+Rate.value+"%."+"</mark>"+"<br>"+"You will receive an amount of Rs."+"<mark>"+final+"</mark>"+","+"<br>"+"in the year "+"<mark>"+Years+"</mark>"+"."
+}
+
+function slider_reader(val){
+    document.getElementById('rate').value=val;
+}
+
         
